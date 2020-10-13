711363312533bc73dd6d2b6166b68c7a17ae6daf
Eddo <eddo@macbookpro-tristan.local>
2020-10-13 10:54:07 +0200

    update movies picture

--- src/data.js
+++ src/data.js
@@ -42,26 +42,26 @@ export default [
   description: 'Le général romain Maximus est le plus fidèle soutien de l\'empereur Marc Aurèle, qu\'il a conduit de victoire en victoire. Jaloux du prestige de Maximus, et plus encore de l\'amour que lui voue l\'empereur, le fils de Marc Aurèle, Commode, s\'arroge brutalement le pouvoir, puis ordonne l\'arrestation du général et son exécution'
 },
 {
-  title: 'Billion', 
-  img: 'https://upload.wikimedia.org/wikipedia/en/5/5c/Billions-KeyArt.jpg', 
+  title: 'Social Dilemma', 
+  img: 'https://secureservercdn.net/50.62.172.113/gvt.c8b.myftpupload.com/wp-content/uploads/2020/08/THE_SOCIAL_DILEMMA_MONSTER_HI_RES_JPEG-scaled.jpg', 
   details: 'R | 175min | Crime, Drama', 
   description: 'Le général romain Maximus est le plus fidèle soutien de l\'empereur Marc Aurèle, qu\'il a conduit de victoire en victoire. Jaloux du prestige de Maximus, et plus encore de l\'amour que lui voue l\'empereur, le fils de Marc Aurèle, Commode, s\'arroge brutalement le pouvoir, puis ordonne l\'arrestation du général et son exécution'
 },
 {
-  title: 'Gladiator', 
-  img: 'https://www.gstatic.com/tv/thumb/v22vodart/24674/p24674_v_v8_ag.jpg', 
+  title: 'Icarus', 
+  img: 'https://fr.web.img6.acsta.net/pictures/17/07/10/10/06/076389.jpg', 
   details: 'R | 175min | Crime, Drama', 
   description: 'Le général romain Maximus est le plus fidèle soutien de l\'empereur Marc Aurèle, qu\'il a conduit de victoire en victoire. Jaloux du prestige de Maximus, et plus encore de l\'amour que lui voue l\'empereur, le fils de Marc Aurèle, Commode, s\'arroge brutalement le pouvoir, puis ordonne l\'arrestation du général et son exécution'
 }, 
 {
-  title: 'The Godfather', 
-  img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRI6-qDqd_iAgSBR_blMT_isLpoxs98q_DpyQ&usqp=CAU', 
+  title: 'The Wolf of Wall Street', 
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91m2MB2lYFL._AC_SY445_.jpg', 
   details: 'R | 175min | Crime, Drama', 
   description: 'Le général romain Maximus est le plus fidèle soutien de l\'empereur Marc Aurèle, qu\'il a conduit de victoire en victoire. Jaloux du prestige de Maximus, et plus encore de l\'amour que lui voue l\'empereur, le fils de Marc Aurèle, Commode, s\'arroge brutalement le pouvoir, puis ordonne l\'arrestation du général et son exécution'
 },
 {
-  title: 'Succession', 
-  img: 'https://images-na.ssl-images-amazon.com/images/I/812y4ExlzmL._AC_SY445_.jpg', 
+  title: 'The Big Short', 
+  img: 'https://lh3.googleusercontent.com/proxy/uA-zoxKAkjEIjCB8UFoHtA88XFJF5LPwUOrLQuUFp8-4MaVwNL23yJpSg_nUbgEjdZSo_otcDlcslnTLbYMwqqFFInqVsSro20e35zcNQVtvGkeQHLFwQ4TCQ98SDSz0oMGEJXaPYbZ1a-GxY78', 
   details: 'R | 175min | Crime, Drama', 
   description: 'Le général romain Maximus est le plus fidèle soutien de l\'empereur Marc Aurèle, qu\'il a conduit de victoire en victoire. Jaloux du prestige de Maximus, et plus encore de l\'amour que lui voue l\'empereur, le fils de Marc Aurèle, Commode, s\'arroge brutalement le pouvoir, puis ordonne l\'arrestation du général et son exécution'
 },
